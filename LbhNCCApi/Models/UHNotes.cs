f4a96188b09e0985e1674fdce9f69c8bf65c8dce
lbhssandilya <33062415+lbhssandilya@users.noreply.github.com>
2019-11-07 12:10:47 +0000

    Replaced Url encoded parameters to request body
    Replaced Url encoded parameters to request body

---
+++ LbhNCCApi/Models/UHNotes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LbhNCCApi.Models
+{
+    public class UHNotes
+    {
+        public string TenancyAgreementId { get; set; }
+        public string Notes { get; set; }
+        public string Username { get; set; }
+    }
+}
